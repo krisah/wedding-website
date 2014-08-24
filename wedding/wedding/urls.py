85574dfc7ce6dba1b27640b0a2a249bab3dc13e4
krisah <krisah0118@gmail.com>
2014-08-24 14:04:25 +0000

    update to urls

--- wedding/wedding/urls.py
+++ wedding/wedding/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, include, url
 from getting_married import views
+
+
+
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wedding.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    #url(r'^$', 'getting_married.views.cover', name='cover'),
-    #url(r'^getting_married', 'getting_married.views.home', name='home'),
     url(r'^$',views.cover,name='cover'),
     url(r'^getting_married/', include('getting_married.urls')),
 )
