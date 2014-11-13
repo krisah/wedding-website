8f06374b792be488be395a50b3f29ee45f2b7a38
krisah0118 <krisah0118@gmail.com>
2014-11-13 18:41:22 -0500

    update email form

--- wedding/wedding/settings.py
+++ wedding/wedding/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 #EMAIL_USE_TLS = True
 #EMAIL_HOST = 'smtp.gmail.com'
 #EMAIL_HOST_USER = 'krisah0118@gmail.com'
-#EMAIL_HOST_PASSWORD ='ly10upu2'
+#EMAIL_HOST_PASSWORD =''
 #EMAIL_PORT = 587
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
